c75b9a8d0f95e2183a187277a9644687dbb502f3
thecotne <cotne.nazarashvili@gmail.com>
2015-03-18 22:03:40 +0400

    fix code style

--- super-subscribe.js
+++ super-subscribe.js
@@ -1,26 +1,26 @@
 (function($) {
-	var decodeHTML = function (html_str) {
+	var decodeHTML = function(html_str) {
 		return $("<div />").html(html_str).text();
 	}
 	$.fn.superSubscribe = function(opts) {
 		var superSubscribe = {
 			wrapper: null,
-			init: function(wrapper, opts){
+			init: function(wrapper, opts) {
 				this.wrapper = wrapper;
-				this.opts = $.extend({},opts);
+				this.opts = $.extend({}, opts);
 				this.action = $(this.wrapper).attr('super-subscribe');
 
 				$(this.wrapper).find('[super-subscribe-submit]').on('click', $.proxy(this.submit, this));
 				$(this.wrapper).find('[super-subscribe-email]').on('keypress', $.proxy(this.detectEnter, this));
 
 			},
-			detectEnter: function(e){
-				if(e.which == 13) {
+			detectEnter: function(e) {
+				if (e.which == 13) {
 					e.preventDefault();
 					this.submit();
 				}
 			},
-			ajaxResponse: function (jqXHR, status, statusMSG) {
+			ajaxResponse: function(jqXHR, status, statusMSG) {
 				console.log(jqXHR.responseText);
 				if (jqXHR.responseText) {
 					try {
@@ -29,7 +29,7 @@
 					if (res.status) {
 						if (res.status == 'ok') {
 							var addClass = 'alert-success';
-						} else if(res.status == 'mail_already_exists') {
+						} else if (res.status == 'mail_already_exists') {
 							var addClass = 'alert-info';
 						} else {
 							var addClass = 'alert-danger';
@@ -38,7 +38,7 @@
 						$(this.wrapper).find('.output')
 							.hide()
 							.removeClass('alert-success alert-info alert-danger')
-							.addClass('alert '+addClass)
+							.addClass('alert ' + addClass)
 							.html(decodeHTML(res.data))
 							.fadeIn();
 						// setTimeout($.proxy(function(e){
@@ -52,7 +52,7 @@
 
 				}
 			},
-			submit: function(e){
+			submit: function(e) {
 				e && e.preventDefault();
 				$.ajax({
 					method: 'post',
@@ -64,22 +64,21 @@
 				});
 			}
 		};
-		if (this.length>1) {
+		if (this.length > 1) {
 			var objects = [];
-			$(this).each(function(){
+			$(this).each(function() {
 				var instance = Object.create(superSubscribe);
-				instance.init(this,opts);
+				instance.init(this, opts);
 				objects.push(instance);
 			});
 			return objects;
 		} else {
 			var instance = Object.create(superSubscribe);
-			instance.init(this,opts);
+			instance.init(this, opts);
 			return instance;
 		}
 	}
-	$(function(){
+	$(function() {
 		$("[super-subscribe]").superSubscribe();
 	});
 })(jQuery);
-
